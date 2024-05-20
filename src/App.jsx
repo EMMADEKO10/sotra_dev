13e1c1f1ed06381ea2264dc9c9c31613062e71b5
Crihojames <crihojames@gmail.com>
2024-05-20 14:39:23 +0100

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button} from 'antd';
+import { useNavigate } from 'react-router-dom';
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div> Sotradon</div>
+      <Button type="primary">Primary Button</Button>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { ConfigProvider } from 'antd'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ConfigProvider
+    theme={{
+      components: {
+        Button : {
+          colorPrimary : '#405138',
+          colorPrimaryBgHover : '#405138',
+          borderRadius : '0px',
+        }
+      }
+    }}
+    >
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>,
+)

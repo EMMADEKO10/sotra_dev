5645d99aa723136a8e13d3bed88113d894d225ac
Crihojames <crihojames@gmail.com>
2024-05-30 18:31:10 +0100

    finition de header libre

--- src/components/Navbars/NavBar.jsx
+++ src/components/Navbars/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 const Navbar = () => {
@@ -21,135 +21,154 @@ const Navbar = () => {
 
   return (
     <nav className="sticky top-0 font-bold bg-white z-50 box-border">
-      <div className="flex flex-row justify-between items-center container mx-auto px-4 md:px-0">
-
-
+      <div className="flex flex-row justify-between items-center container mx-auto px-4 py-2 md:px-0 border-bottom border-4 border-[#3bcf94]">
         {/* Logo affiché sur les écrans de taille moyenne et plus */}
-        <div className="h-100 lg:mr-5 xs:block">
+        <div className=" lg:mr-5 ">
           <Link to="/">
+          <div className="sotradons">
             <img
-              src="assets\img\Sotradons.png"
+              src="assets\img\sotradon logo.png"
               alt="logo"
-              style={{ width: "120px", height: "90px" }}
+              style={{ width: "60px", height: "60px" }}
+              className="pr-1"
             />
+            <span className="s">S</span>
+            <span className="o">o</span>
+            <span className="t">t</span>
+            <span className="r">r</span>
+            <span className="a">a</span>
+            <span className="d">d</span>
+            <span className="o">o</span>
+            <span className="n">n</span>
+            <span className="s">s</span>
+          </div>
           </Link>
         </div>
-        
+
         {/* Menu de navigation principal pour les grands écrans */}
         <div className="hidden xl:block">
-            <ul className="flex flex-row justify-between items-center bg-white p-4 uppercase">
-                {[
-                    {
-                        title: "Projets",
-                        subItems: [ "Découvrer les projets", "Démarrer un projet",],
-                    },
+          <ul className="flex flex-row justify-between items-center bg-white  uppercase">
+            {[
+              {
+                title: "Projets",
+                subItems: ["Découvrer les projets", "Démarrer un projet"],
+              },
 
-
-                    {
-                        title: "Info",
-                        subItems: [ "Devenir prestataire","Social bonds","Charte","Blog",],
-                    },
-                    {
-                        title: "Sponsor",
-                        subItems: ["Nos sponsors", "Devenir sponsor"],
-                    },
-                    {
-                        title: "Sotradons",
-                        subItems: ["À propos", "Vision et Mission", "Contact"],
-                    },
-
-                ].map((item, index) => (
-                    <li key={index} className="px-3 py-4 whitespace-nowrap uppercase relative">
-                        <button onClick={() => toggleDropdown(index)} className="uppercase" >
-                        <span className="hover:text-primary">{item.title}</span>
-                        <span className="text-primary"> + </span>
-                        </button>
-                        {/* Dropdowns pour les éléments de navigation */}
-                        {dropdownOpen === index && (
-                        <div className="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow mt-2">
-                            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
-                                {item.subItems.map((subItem, subIndex) => (
-                                    <li key={subIndex} className="hover:text-primary normal-case">
-                                    <a className="block px-4 py-2 dark:hover:bg-gray-600" href="#">
-                                        {subItem}
-                                    </a>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                        )}
-                    </li>
-                ))}
-            </ul>
-        </div>
-            {/* Call action */}
-            <div className="w-full xs:hidden flex items-right justify-between py-2">
-            <a
-                href="#"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="whitespace-nowrap"
-            >
+              {
+                title: "Info",
+                subItems: [
+                  "Devenir prestataire",
+                  "Social bonds",
+                  "Charte",
+                  "Blog",
+                ],
+              },
+              {
+                title: "Sponsor",
+                subItems: ["Nos sponsors", "Devenir sponsor"],
+              },
+              {
+                title: "Sotradons",
+                subItems: ["À propos", "Vision et Mission", "Contact"],
+              },
+            ].map((item, index) => (
+              <li
+                key={index}
+                className="px-3 whitespace-nowrap uppercase relative"
+              >
                 <button
-                className="bg-primary hover:bg-white hover:text-primary text-white border-primary border-2 flex flex-row items-center ease-linear duration-200 px-3 text-lg flex items-center gap-x-2 font-normal py-1.5 rounded-md"
-                type="button"
+                  onClick={() => toggleDropdown(index)}
+                  className="uppercase bg-transparent hover:text-[#282f68] transition duration-300 ease-in-out font-bold"
                 >
-                <span>Connexion</span>
+                  <span className="hover:text-primary">{item.title}</span>
+                  <span className="text-primary"> + </span>
                 </button>
-            </a>
-            </div>
+                {/* Dropdowns pour les éléments de navigation */}
+                {dropdownOpen === index && (
+                  <div className="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow mt-2">
+                    <ul className="py-2 text-ls text-gray-700 dark:text-gray-200">
+                      {item.subItems.map((subItem, subIndex) => (
+                        <li
+                          key={subIndex}
+                          className="hover:text-primary normal-case"
+                        >
+                          <a
+                            className="block px-4 py-2 dark:hover:bg-[#3bcf946e]"
+                            href="#"
+                          >
+                            {subItem}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
 
+        {/* Call action */}
+        <div className="hidden md:block">
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="whitespace-nowrap"
+          >
+            <button
+              className="bg-primary hover:bg-white hover:text-primary text-white border-primary border-2 flex flex-row items-center ease-linear duration-200 px-3 text-lg flex items-center gap-x-2 font-normal py-1.5 rounded-md"
+              type="button"
+            >
+              <span>Connexion</span>
+            </button>
+          </a>
+        </div>
 
         {/* Section pour l'inscription et le menu burger sur les petits écrans */}
-        <div className="flex flex-row justify-between items-center">
+        <div className="flex flex-row justify-between items-center xl:hidden">
           {/* Bouton du menu mobile */}
-          <div className="ml-3 xl:hidden">
+          <div className="ml-3">
             <button
-              className="cursor-pointer"
+              className="cursor-pointer bg-transparent "
               type="button"
               onClick={() => setMenuOpen(!menuOpen)}
             >
-              <div className="block w-6 h-1 mx-1 my-1 bg-black ease-linear duration-75 rounded-lg"></div>
-              <div className="block w-6 h-1 mx-1 my-1 bg-black ease-linear duration-75 rounded-lg"></div>
-              <div className="block w-6 h-1 mx-1 my-1 bg-black ease-linear duration-75 rounded-lg"></div>
+              <div className="block w-6 h-1 mx-1 my-1 bg-primary ease-linear duration-75 rounded-lg"></div>
+              <div className="block w-6 h-1 mx-1 my-1 bg-primary ease-linear duration-75 rounded-lg"></div>
+              <div className="block w-6 h-1 mx-1 my-1 bg-primary ease-linear duration-75 rounded-lg"></div>
             </button>
           </div>
         </div>
       </div>
+
       {/* Menu mobile affiché quand menuOpen est vrai */}
       {menuOpen && (
         <div className="bg-gray w-full absolute overflow-hidden ease-linear duration-100 xl:hidden min-h-screen">
           <div className="bg-gray w-full ease-linear duration-100 xl:hidden absolute min-h-screen">
             {[
               {
-                title: "Formations",
-                subItems: [
-                  "Bootcamp carrière",
-                  "Kadea Boost",
-                  "Kadea Online",
-                  "Certificat Executive",
-                ],
+                title: "Projets",
+                subItems: ["Découvrer les projets", "Démarrer un projet"],
               },
-              { title: "Campus", subItems: ["Kinshasa", "Goma", "Lubumbashi"] },
+
               {
-                title: "Découvrir",
+                title: "Info",
                 subItems: [
-                  "À propos",
-                  "Emploi à la clé",
-                  "Réalisations",
-                  "Carrière",
-                  "Frais académiques",
+                  "Devenir prestataire",
+                  "Social bonds",
+                  "Charte",
+                  "Blog",
                 ],
               },
               {
-                title: "Entreprises",
-                subItems: ["Mentor4Job", "Kadea Software"],
+                title: "Sponsor",
+                subItems: ["Nos sponsors", "Devenir sponsor"],
               },
               {
-                title: "Vodacom Lab",
-                subItems: ["Vodacom Digital Lab", "Kadea Online"],
+                title: "Sotradons",
+                subItems: ["À propos", "Vision et Mission", "Contact"],
               },
-              { title: "Communauté", subItems: ["State of Dev"] },
             ].map((item, index) => (
               <div
                 key={index}
@@ -191,7 +210,7 @@ const Navbar = () => {
                             className="hover:text-primary normal-case"
                           >
                             <a
-                              className="block px-4 py-2 dark:hover:bg-gray-600"
+                              className="block px-4 py-2 dark:hover:bg-[#3bcf946e]"
                               href="#"
                             >
                               {subItem}
@@ -208,7 +227,7 @@ const Navbar = () => {
             {/* Bouton d'inscription dans le menu mobile */}
             <div className="py-5 xs:hidden container mx-auto px-4 md:px-0">
               <a
-                href="https://forms.fillout.com/t/kb72R3kuNfus"
+                href=""
                 target="_blank"
                 rel="noopener noreferrer"
                 className="whitespace-nowrap"
@@ -217,7 +236,7 @@ const Navbar = () => {
                   className="bg-primary hover:bg-white hover:text-primary text-white border-primary border-2 flex flex-row items-center ease-linear duration-200 px-3 text-lg flex items-center gap-x-2 font-normal py-1.5 rounded-md"
                   type="button"
                 >
-                  <span>Inscris-toi</span>
+                  <span>Connexion</span>
                 </button>
               </a>
             </div>

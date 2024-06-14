6bb541498fa6a9d885887b32f36cf032a24e2c13
EMMADEKO10 <emmanueldeko64@gmail.com>
2024-06-14 17:03:45 +0100

    Ajout des messages de succès lors de l'enregistrement

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,11 +3,11 @@ import { Link, useNavigate } from 'react-router-dom';
 import Divider from '../../components/Divider';
 import axios from 'axios';
 import Navbar from '../../components/Navbars/NavBar';
-
+import { useState } from 'react';
 
 function Login() {
   const navigate = useNavigate();
-
+  const [isRegistrationSuccessful, setIsRegistrationSuccessful] = useState(false); // Add state for success message
   const onFinish = async (value) => {
     console.log('Received values of form: ', value);
 
@@ -22,13 +22,10 @@ function Login() {
       localStorage.setItem("token", response.data.token);
       localStorage.setItem("user", response.data.user._id)
       localStorage.setItem("role", response.data.user.role)
-
-
       console.log("voici la reponse", response.data.user._id)
 
-
       if (response.status === 200 || response.status === 201) {
-       
+        setIsRegistrationSuccessful(true);
         setTimeout(() => {
           navigate('/');
         },3000);
@@ -100,6 +97,21 @@ function Login() {
                 Connexion
               </Button>
             </Form.Item>
+
+            {isRegistrationSuccessful && (
+              <div className="bg-green-100 p-5 w-[100%] sm:w-1/2 rounded mb-10 items-center">
+                <div className="flex justify-between">
+                  <div className="flex space-x-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="flex-none fill-current text-green-500 h-4 w-4">
+                      <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.25 16.518l-4.5-4.319 1.396-1.435 3.078 2.937 6.105-6.218 1.421 1.409-7.5 7.626z" /></svg>
+                    <div className="flex-1 leading-tight text-sm text-green-700 font-medium">succès</div>
+                  </div>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="flex-none fill-current text-green-600 h-3 w-3">
+                    <path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z" /></svg>
+                </div>
+              </div>
+            )}
+
 
             <div className='text-center'>
               <span className='text-gray-500'>

b5f62bc67862dedfbce1b91ea4d0f35962091242
Crihojames <crihojames@gmail.com>
2024-06-20 10:33:28 +0100

    deboge build

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import styleImport from 'vite-plugin-style-import';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    reactRefresh(),
-    styleImport({
-      libs: [{
-        libraryName: 'antd',
-        esModule: true,
-        resolveStyle: (name) => `antd/es/${name}/style/index`
-      }]
-    })
-  ]
-});
+  plugins: [react()],
+})
